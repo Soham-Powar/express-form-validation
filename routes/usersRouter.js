bb839eaa256a94f2cca8a5b3bd6cacf965138971
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-08-06 11:25:20 +0530

    feat: add the update user functionality

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -5,5 +5,7 @@ const usersController = require("../controllers/usersController");
 usersRouter.get("/", usersController.usersListGet);
 usersRouter.get("/create", usersController.usersCreateGet);
 usersRouter.post("/create", usersController.usersCreatePost);
+usersRouter.get("/:id/update", usersController.usersUpdateGet);
+usersRouter.post("/:id/update", usersController.usersUpdatePost);
 
 module.exports = usersRouter;
